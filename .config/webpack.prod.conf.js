023fb0edee809ad06435508c9100accbfc951365
haozime <syntaxright@gmail.com>
2017-03-18 22:46:20 +0800

    init

--- .config/webpack.prod.conf.js
+++ .config/webpack.prod.conf.js
@@ -8,6 +8,8 @@ const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Manifest = require('webpack-manifest')
+
 const env = process.env.NODE_ENV === 'testing'
   ? require('config/test.env')
   : config.build.env
@@ -73,6 +75,32 @@ const webpackConfig = merge(baseWebpackConfig, {
       },
       chunksSortMode: 'dependency'
     }),
+
+    new Manifest({
+      cache: [
+        '/[hash:3].js',
+        'https://s4.cnzz.com/z_stat.php?id=1261523779&web_id=1261523779'
+      ],
+      // Add time in comments.
+      timestamp: true,
+      // 生成的文件名字，选填
+      // The generated file name, optional.
+      filename: 'cache.manifest',
+      // 注意*星号前面用空格隔开
+      network: [
+        '*'
+      // 'http://api.map.baidu.com/ *',
+      // 'http://p.ssl.qhimg.com/ *',
+      // 'https://p.ssl.qhimg.com/ *'
+      ],
+      // 注意中间用空格隔开
+      // fallback: ['/ /404.html'],
+      // manifest 文件中添加注释
+      // Add notes to manifest file.
+      headcomment: pkg.name + ' v' + pkg.version,
+      master: ['index/index.html']
+    }),
+
     config.build.vendor && new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       minChunks: function (module, count) {
