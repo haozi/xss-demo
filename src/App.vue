de7cf54763a62e60e49c00e1e3549e9b92436ef8
haozime <syntaxright@gmail.com>
2017-03-19 13:01:53 +0800

    feat: 🎸 增加编码工具

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
+<style lang="less">
+  @import url('./style/main.less');
+</style>
+
 <template>
   <div id="app">
     <div v-if="isWeixin" style="display:none"><img src="//p.ssl.qhimg.com/dm/500_500_/t01a98be2ae4a0c95bf.jpg"></div>
     <router-view></router-view>
+    <div class="i-footer">
+      <a href="/tools/xss-encode" target="_blank">编码工具</a>
+    </div>
   </div>
 </template>
 
