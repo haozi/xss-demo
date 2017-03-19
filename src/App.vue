251ebaf8959456f12082ecd5aca442ca0c95a2bc
haozime <syntaxright@gmail.com>
2017-03-19 15:08:15 +0800

    增加safe-trim 链接

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
     <div v-if="isWeixin" style="display:none"><img src="//p.ssl.qhimg.com/dm/500_500_/t01a98be2ae4a0c95bf.jpg"></div>
     <router-view></router-view>
     <div class="i-footer">
-      <a href="/tools/xss-encode" target="_blank">编码工具</a>
+      <a href="https://xss.haozi.me/tools/xss-encode" target="_blank">编码工具</a> |
+      <a href="https://github.com/haozime/safe-trim" target="_blank">Safe-Trim</a>
     </div>
   </div>
 </template>
