077bc895eeae708cc728738d45d16c42861dd5d1
haozime <syntaxright@gmail.com>
2017-03-22 11:24:22 +0800

    增加题目

--- src/data/exam/_102.alf nu.js
+++ src/data/exam/_102.alf nu.js
@@ -1,9 +1,7 @@
 // from alf.nu
 function escape (s) {
   let text = s.replace(/</g, '&lt;').replace(/"/g, '&quot;')
-  // URLs
   text = text.replace(/(http:\/\/\S+)/g, '<a href="$1">$1</a>')
-  // [[img123|Description]]
-  text = text.replace(/\[\[(\w+)\|(.+?)\]\]/g, '<img alt="$2" src="$1.gif">')
+  text = text.replace(/\[\[(\w+)\|(.+?)]]/g, '<img alt="$2" src="$1.gif">')
   return text
 }
