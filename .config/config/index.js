084d3e401b6e28c3ee84a0329690359a70b27e26
haozi <syntaxright@gmail.com>
2018-01-02 15:11:27 +0800

    add gh-pages

--- .config/config/index.js
+++ .config/config/index.js
@@ -10,7 +10,7 @@ module.exports = {
     index: path.resolve(__dirname, '../../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../../dist'),
     assetsSubDirectory: './',
-    assetsPublicPath: '',
+    assetsPublicPath: 'https://haozi.github.io/xss-demo/dist/',
     productionSourceMap: false,
     productionGzip: false,
     linkCss: false,
